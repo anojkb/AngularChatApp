fa48b55b619441c47459c0a12a133817b77b87a9
anojkb <anojkb.ui@gmail.com>
2025-01-01 19:07:18 +0530

    register css

--- angular-messaging-app/src/app/components/register/register.component.ts
+++ angular-messaging-app/src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,24 +10,29 @@ import { Router } from '@angular/router';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent implements OnInit{
-  registerForm: FormGroup; 
+  registerForm !: UntypedFormGroup; 
   hidePassword = true; 
   username: string = ''; 
   password: string = ''; 
   confirmPassword: string = ''; 
   hideConfirmPassword = true;
-  constructor(private fb: FormBuilder,private router: Router) {
-  } 
+  // passwordMatchValidator: any;
+
+  constructor(private fb: UntypedFormBuilder,private router: Router) {  } 
 
   ngOnInit(): void {
-    this.registerForm = this.fb.group({ 
-      username: ['', [Validators.required, this.noWhitespaceValidator]], 
-      password: [ '', [ Validators.required, Validators.minLength(6), 
-          Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-= \[\] {};':"\\|,.<>\/?]).{6,}$/) 
-        ] 
-      ], 
-      confirmPassword: ['', [Validators.required]] }, { validator: this.passwordMatchValidator });
-  }
+    this.registerForm = this.fb.group({
+        username: ['', [Validators.required, this.noWhitespaceValidator]],
+        password: ['', [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{6,}$/)
+        ]],
+        confirmPassword: ['', [Validators.required]]
+    }, { validators: this.passwordMatchValidator });
+}
+
+
   onSubmit(): void { 
     if (this.registerForm.valid) { 
       const { username, password } = this.registerForm.value; 
@@ -37,16 +42,22 @@ export class RegisterComponent implements OnInit{
     switchToLoginPage(): void { 
       this.router.navigate(['/login']); 
     } 
-    noWhitespaceValidator(control) { 
+
+    noWhitespaceValidator(control: { value: any; }) { 
       const isWhitespace = (control.value || '').trim().length === 0; const isValid = !isWhitespace; return isValid ? null : { 'noWhitespace': true }; 
     } 
-    passwordMatchValidator(formGroup: FormGroup) { 
-      
-      const password = formGroup.get('password').value; 
-      const confirmPassword = formGroup.get('confirmPassword').value; 
-      if (password !== confirmPassword) { 
-        formGroup.get('confirmPassword').setErrors({ mismatch: true }); } else { return null; } 
-      } 
+
+    passwordMatchValidator(formGroup: UntypedFormGroup) {
+      const password = formGroup.get('password')?.value;
+      const confirmPassword = formGroup.get('confirmPassword')?.value;
+      if (password !== confirmPassword) {
+        formGroup.get('confirmPassword')?.setErrors({ mismatch: true });
+      } else {
+        formGroup.get('confirmPassword')?.setErrors(null);
+      }
+      return null; // Ensure a return value is always provided
+    }
+
       togglePasswordVisibility() { 
         this.hidePassword = !this.hidePassword; 
       }
